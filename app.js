b59e6d7cf860c3fd118af23422f054b5970f39d4
YusraImran <sheikhxheikh692@gmail.com>
2023-05-21 22:51:48 -0500

    assigment

---
+++ app.js
@@ -0,0 +1,21 @@
+function getNum(num) {
+    console.log(num)
+    document.getElementById('result').value+=num;
+    
+}
+
+function clearScr(){
+    document.getElementById('result').value ="";
+}
+
+    function removeNum(){
+        var res = document.getElementById('result').value;
+        document.getElementById('result').value = document.getElementById('result').value.slice(0,res.length-1);
+    }    
+    function getAns(){
+        document.getElementById('result').value = eval(document.getElementById('result').value)
+    }
+function myFunction(){
+    var element = document.body;
+    element.classList.toggle("darkmode");
+}
